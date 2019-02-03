edc71f605ae93f0769df0bd27a5c2c1ec1505813
jakeminns <jm648@kent.ac.uk>
2019-02-03 15:58:04 +0000

    Update Working

--- Neural_Network.py
+++ Neural_Network.py
@@ -59,8 +59,6 @@ def gradientChecking(a_lj,theta,y,lambda_1,m,net_top):
 	return grad_check 
 
 
-
-
 def numOutputPrediction(x):
 	for i in range(0,len(x)):
 		if(x[i]>0.7):
@@ -85,15 +83,19 @@ class Network:
 		self.a_lj = self.networkConstruct()
 
 	def classificaionContour(self,xmin,xmax,step):
-		contour = np.zeros((np.abs(xmin)+xmax)*(np.abs(xmin)+xmax))
+		#contour = np.zeros(np.divide((np.abs(xmin)+xmax),step)*np.divide((np.abs(xmin)+xmax),step))
+		contour = np.zeros(int((2/0.1)*(2/0.1)))
+
 		a_lj = self.networkConstruct()
 		counter = 0
-		for x in np.arange(xmin,xmax,1):
-			for y in np.arange(xmin,xmax,1):
-				ar = np.array([1.0,np.divide(x,100.0),np.divide(y,100.0),np.divide(x*x,100.0),np.divide(y*y,100.0)])
+
+		for x in np.arange(xmin,xmax,step):
+			for y in np.arange(xmin,xmax,step):
+				ar = np.array([x,y])
 				contour[counter] = self.forwardProp(ar)[len(self.net_top)-1]
 				counter+=1
-		return np.transpose(np.reshape(contour,(200,200)))
+
+		return np.transpose(np.reshape(contour,(20,20)))
 
 
 
@@ -101,14 +103,14 @@ class Network:
 		#This is a list not numpy array may cause problems later.
 		theta = []
 		for i in range(1,len(self.net_top)):
-			theta.append(np.random.rand(self.net_top[i],self.net_top[i-1]))
-			theta[i-1] = np.insert(theta[i-1], 0, self.bias, axis=1)
+			theta.append(np.random.rand(self.net_top[i],self.net_top[i-1]+1))
 		return theta
 
 	def deltaInit(self):
-		delta = []
+		delta = []  
 		for i in range(1,len(self.net_top)):
 			delta.append(np.zeros((self.net_top[i],self.net_top[i-1]+1,)))
+
 		return delta
 
 	def sigInit(self):
@@ -120,6 +122,7 @@ class Network:
 	def networkConstruct(self):
 		a_lj = []
 		#Input
+		print(self.net_top[0])
 		a_lj.append(np.ones(self.net_top[0]))
 		#Hidden
 		for i in range(1,len(self.net_top)-1):
@@ -128,8 +131,12 @@ class Network:
 		a_lj.append(np.ones(self.net_top[len(self.net_top)-1]))
 		return a_lj
 	
-	def sigmoid(self,theta,x):
+	def sigmoid_(self,theta,x):
 		return 1/(1+np.exp(np.sum((-1*theta*x),axis=1)))
+
+	def sigmoid(self,theta,x):
+		exp = np.sum((theta*x),axis=1)
+		return (np.exp(exp)-np.exp(-1*exp))/(np.exp(exp)+np.exp(-1*exp))
 
 	def costFuncLogisticReg(self,y):
 
@@ -139,7 +146,8 @@ class Network:
 		#log(1-x) tends to -inf at x = 1, python put this as -inf so filter to replace this with large number
 		OneLogSig[OneLogSig==np.log(0)] = -10000000000000
 		logSig[OneLogSig==np.log(0)] = -100000000000000
-		regularizationTerm = (self.lambda_1/(2*self.m))*np.sum(self.theta[1:])
+		
+		#regularizationTerm = (self.lambda_1/(2*self.m))*np.sum(self.theta[1:])
 		#s_i = np.sum((-y[img][1]*logSig)-(1-y[img][1])*OneLogSig)
 		s_i = np.sum((-y*logSig)-(1-y)*OneLogSig)
 		return s_i
@@ -148,14 +156,20 @@ class Network:
 		return self.a_lj[len(a_lj)-1]
 
 	def forwardProp(self,x):
+
 		self.a_lj[0] = x 
+		#print("theta",self.theta)
 		for i in range(1,len(self.theta)+1):
-			new_a_lj = self.sigmoid(self.theta[i-1],self.a_lj[i-1])
 			#Add Bias to all except output
-			if(i!=len(self.theta)):
-				new_a_lj = np.insert(new_a_lj, 0, self.bias, axis=0)
+			a_lj_i = self.a_lj[i-1]
+			if(i!=len(self.theta)+1):
+				a_lj_i = np.insert(a_lj_i, 0, self.bias, axis=0)
+
+			new_a_lj = self.sigmoid(self.theta[i-1],a_lj_i)
+
 			#Updte a_lj
 			self.a_lj[i] = new_a_lj
+
 		return self.a_lj
 	
 	def backProp(self,y,delta):
@@ -164,39 +178,41 @@ class Network:
 		dif = self.deltaInit()
 		
 		# -1 so using 0 works
-		netLen = len(self.a_lj)-1 
+		netLen = len(self.a_lj)-1
+
 		#Output layer
 		sig[len(sig)-1] = -1.0*(y-self.a_lj[netLen])*np.multiply((self.a_lj[len(self.a_lj)-1]),(1.0-self.a_lj[len(self.a_lj)-1]))
-		#Remove Bias layer in theta and a_lj (a_lj removal skips last entry as output has no bias)
 		
+		#Remove Bias layer in theta 
 		theta_NoBias = []
 		for i in range(len(self.theta)):
 			temp = self.theta[i].copy()
 			theta_NoBias.append(np.delete(temp,0,axis=1))
 
-		a_lj_NoBias = []
-		for i in range(0,len(self.a_lj)-1):
-			temp = self.a_lj[i].copy()
-			a_lj_NoBias.append(np.delete(temp,0,axis=0))
-
 		for l in range(len(sig)-1,0,-1):
-			a1 = (np.transpose(theta_NoBias[l]).dot(sig[l]))		
-			a2 =  np.multiply((a_lj_NoBias[l]),(1.0-a_lj_NoBias[l]))
+			a1 = (np.transpose(theta_NoBias[l]).dot(sig[l]))	
+			a2 =  np.multiply(self.a_lj[l],(1.0-self.a_lj[l]))
 			a3 = np.multiply(a1 , (a2))
 			sig[l-1] = a3
 
 		for i in range(len(delta)-1,-1,-1):
-			trans = np.transpose(self.a_lj[i])
+			a_lj_i = np.insert(self.a_lj[i], 0, self.bias, axis=0)
+			trans = np.transpose(a_lj_i)
 			outer = np.outer(sig[i],trans)
 			delta[i] = (delta[i] +outer)
 
 		return delta
 
 	def updateTheta(self):
+
 		for i in range(0,len(self.theta),1):	
 			regSum = self.lambda_1*np.sum(self.theta[i][1:len(self.theta[i])-1])
-			#Reg term shgould acount for j=0 and j!=0
-			theta_temp = self.theta[i] - self.alpha*(1.0/self.m)*(self.delta[i]+np.multiply(self.lambda_1,self.theta[i]))
+			
+			#Reg term acounts for j=0 and j!=0
+			theta_temp_j = self.theta[i][:,1:] - self.alpha*(1.0/self.m)*(self.delta[i][:,1:]+np.multiply(self.lambda_1,self.theta[i][:,1:]))			
+			theta_temp_jZero = self.theta[i][:,0] - self.alpha*(1.0/self.m)*(self.delta[i][:,0])
+			theta_temp = np.insert(theta_temp_j,0,theta_temp_jZero,axis=1)
+
 			self.theta[i] = theta_temp
 	
 	def trainNN(self,x_train,y_train,cycles):
@@ -210,8 +226,41 @@ class Network:
 				self.delta = self.backProp(y_train[m_i],self.delta)
 				cost_i = self.costFuncLogisticReg(y_train[m_i])
 				cost.append(cost_i)
-			print("Cost:",(0.5/self.m)*np.sum(cost))
+			print("Epoch",c,"Loss:",(0.5/self.m)*np.sum(cost))
+
+			if (c%10==0):
+				contour = NN.classificaionContour(-1,1,0.1)
+				plt.imshow(contour, cmap='rainbow', interpolation='nearest')
+				plt.pause(0.05)
 			self.updateTheta()
+			
+		plt.show()
+
+
+from sklearn.datasets import make_circles
+import matplotlib.pyplot as plt
+
+dataset = make_circles(n_samples=500,noise=0.1,factor=0.5, random_state=1)
+
+features = dataset[0]
+print("Features:",features[:10])
+labels = dataset[1]
+print("Labels:",labels[:10])
+
+labels = labels.reshape(labels.shape[0],1)
+
+data_len = features.shape[0]
+
+x1_min = np.amin(features[:,0])
+x1_max = np.amax(features[:,0])
+x2_min = np.amin(features[:,1])
+x2_max = np.amax(features[:,1])
+
+X_train = features[:int(data_len*0.7)]
+X_test = features[int(data_len*0.3):]
+y_train = labels[:int(data_len*0.7)]
+y_test = labels[int(data_len*0.3):]
+
 
 ###################################################
 
@@ -219,14 +268,15 @@ class Network:
 
 ###################################################
 
-NN = Network(4,[4,4,1],1,1,1)
 
-x1,x2,y_data = np.genfromtxt('ex2data2.txt',delimiter=',',unpack=True)
-x_data = np.array((np.ones(len(x1)),np.divide(x1,100.0),np.divide(x2,100.0),np.divide(x2*x2,100.0),np.divide(x1*x1,100.0)))
-x_data = np.transpose(x_data)
+NN = Network(2,[2,6,1],30,1,0.0)
 
-NN.trainNN(x_data,y_data,100)
+insert_ = X_train[:,[0]]*X_train[:,[0]]
+#X_train = np.append(X_train,insert_,axis=1)
 
-contour = NN.classificaionContour(-100,100,1)
+NN.trainNN(X_train,y_train,1000)
+
+contour = NN.classificaionContour(-1.0,1.0,0.1)
+
 plt.imshow(contour, cmap='rainbow', interpolation='nearest')
 plt.show()
